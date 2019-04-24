27bc45d9d7dba37de0324c1de57177265dd4a453
feng <hggfjt@126.com>
2019-04-24 23:31:06 +0800

    前4，了解go并发概述

---
+++ src/1_3_17.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var data int
+
+	go func() {
+		data++
+	}()
+
+	if data == 0 {
+		fmt.Printf("the value is 0.")
+	} else {
+		fmt.Printf("the value is %v.\n", data)
+	}
+}
